38e18c9ebebef654f70692fed6c8c43e65f1b69d
duttod <drissdutto@hotmail.fr>
2019-10-10 15:15:48 +0200

    Commit crédit

--- app/src/main/java/com/example/softwar/controllers/MainActivity.java
+++ app/src/main/java/com/example/softwar/controllers/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.softwar.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.softwar.R;
 
@@ -11,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ChargerPartie(View view) {
+    }
+
+    public void afficherCredit(View view) {
+        Intent intent = new Intent(this,CreditActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void Nouvelle_Partie(View view) {
     }
 }
