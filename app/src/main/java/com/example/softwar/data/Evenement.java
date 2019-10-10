efa481f3316c6d91cdc1956459de3dde6dab1af1
kemplail <floy737@gmail.com>
2019-10-10 14:46:45 +0200

    Test

---
+++ app/src/main/java/com/example/softwar/data/Evenement.java
@@ -0,0 +1,104 @@
+package com.example.softwar.data;
+
+public class Evenement {
+
+	private int nbUtilisateurs;
+	private int argent;
+	private int productivite;
+	private int securite;
+	private int puissance;
+	private int ergonomie;
+	private int attribute;
+
+	public void getNbUtilisateurs() {
+		// TODO - implement com.example.softwar.data.Evenement.getNbUtilisateurs
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param nbUtilisateurs
+	 */
+	public void setNbUtilisateurs(int nbUtilisateurs) {
+		this.nbUtilisateurs = nbUtilisateurs;
+	}
+
+	public void getArgent() {
+		// TODO - implement com.example.softwar.data.Evenement.getArgent
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param argent
+	 */
+	public void setArgent(int argent) {
+		this.argent = argent;
+	}
+
+	public void getProductivite() {
+		// TODO - implement com.example.softwar.data.Evenement.getProductivite
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param productivite
+	 */
+	public void setProductivite(int productivite) {
+		this.productivite = productivite;
+	}
+
+	public void getSecurite() {
+		// TODO - implement com.example.softwar.data.Evenement.getSecurite
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param securite
+	 */
+	public void setSecurite(int securite) {
+		this.securite = securite;
+	}
+
+	public void getPuissance() {
+		// TODO - implement com.example.softwar.data.Evenement.getPuissance
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param puissance
+	 */
+	public void setPuissance(int puissance) {
+		this.puissance = puissance;
+	}
+
+	public void getErgonomie() {
+		// TODO - implement com.example.softwar.data.Evenement.getErgonomie
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param ergonomie
+	 */
+	public void setErgonomie(int ergonomie) {
+		this.ergonomie = ergonomie;
+	}
+
+	public void getAttribute() {
+		// TODO - implement com.example.softwar.data.Evenement.getAttribute
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param attribute
+	 */
+	public void setAttribute(int attribute) {
+		this.attribute = attribute;
+	}
+
+}
--- app/src/test/java/com/example/softwar/ExampleUnitTest.java
+++ app/src/test/java/com/example/softwar/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.softwar;
+pacctly answer you question. You would be able to attain the behavior you want by copying your current layout xml file into a new folder titled "layout-land" and changing the necessary attributes of the LinearLayout. This is the folder that will be used by Android to load your layout files if the device is in landscape mode. If it cannot find a given xml file in the "layout-land" folder then it will default to the "layout" folder.kage com.example.softwar;
 
 import org.junit.Test;
 
