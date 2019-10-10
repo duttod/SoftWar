92c2392a1324394c6265522938ca1e71ce9910c8
kemplail <floy737@gmail.com>
2019-10-10 14:41:42 +0200

    JTM DRISS

--- app/src/main/java/com/example/softwar/controllers/MainActivity.java
+++ app/src/main/java/com/example/softwar/controllers/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.softwar;
+package com.example.softwar.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.softwar.R;
 
 public class MainActivity extends AppCompatActivity {
 
