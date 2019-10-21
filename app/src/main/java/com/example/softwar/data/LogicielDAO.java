62a0531c8deef9cb42cdc1550c58927a452242a4
duttod <drissdutto@hotmail.fr>
2019-10-21 10:12:44 +0200

    room solution

---
+++ app/src/main/java/com/example/softwar/data/LogicielDAO.java
@@ -0,0 +1,14 @@
+package com.example.softwar.data;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+import com.example.softwar.data.Entreprise;
+
+
+import java.util.List;
+
+@Dao
+public interface LogicielDAO {
+}
