2b9953c2d19d499eee66c668bb6e906fb751e3d7
mirasl <lucas.miras@live.fr>
2019-10-21 09:23:20 +0200

    add un import dans choose renforcer..

--- app/src/main/java/com/example/softwar/controllers/ChooseRenforcerAttaquerActivity.java
+++ app/src/main/java/com/example/softwar/controllers/ChooseRenforcerAttaquerActivity.java
@@ -2,6 +2,7 @@ package com.example.softwar.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.softwar.R;
 
 public class ChooseRenforcerAttaquerActivity extends AppCompatActivity {
 
